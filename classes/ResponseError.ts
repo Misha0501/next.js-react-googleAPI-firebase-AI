031710962a276c8bf28532c72ac8c1623fff34f3
mykhailogalenda <misha.galenda@gmail.com>
2023-08-25 12:59:43 +0200

    added reusable response error

---
+++ classes/ResponseError.ts
@@ -0,0 +1,10 @@
+export class ResponseError extends Error {
+    constructor(message, status) {
+        super(message);
+        this.name = 'ResponseError';
+        this.status = status || 500;
+        // Capturing stack trace, excluding constructor call from it
+        // (this is probably no longer required in node >=8, see the comments)
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
