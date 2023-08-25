5d1fe78a21b556bf77425b7a07e004678dec6570
mykhailogalenda <misha.galenda@gmail.com>
2023-08-25 13:02:40 +0200

    added route to get current user

---
+++ app/api/user/route.ts
@@ -0,0 +1,35 @@
+import {NextResponse} from 'next/server'
+import {PrismaClient} from '@prisma/client'
+import {z} from "zod"
+import {getApplicationUserServer} from "@/app/lib/getApplicationUserServer";
+import {ResponseError} from "@/classes/ResponseError";
+
+const prisma = new PrismaClient();
+
+/**
+ * POST Route to register new users. it's only invoked be Google functions
+ * @param req
+ * @constructor
+ */
+export async function GET(req: Request) {
+    try {
+        const user = await getApplicationUserServer();
+        return NextResponse.json(user)
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return new Response(error.message, {status: 422})
+        }
+
+        if(error instanceof ResponseError) {
+            return new Response(error.message, {status: error.status})
+        }
+
+        if(error.errorInfo && error.errorInfo.code) {
+            return new Response('Firebase ID token is invalid or it has expired. Get a fresh ID token and try again.', {status: 400})
+        }
+
+        return new Response('Something went wrong please try again later', {
+            status: 500,
+        })
+    }
+}
