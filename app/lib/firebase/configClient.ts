ce3c1108a8687373709083abe3ec9ab1fd210176
mykhailogalenda <misha.galenda@gmail.com>
2023-08-29 22:38:03 +0200

    updated env setup

--- app/lib/firebase/configClient.ts
+++ app/lib/firebase/configClient.ts
@@ -17,10 +17,15 @@ const firebaseConfig = {
 export let firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const firebaseClientAuth = getAuth(firebaseApp);
 
-// if (process.env.NODE_ENV === 'development') {
-//     // Use emulator if developing
-//     connectAuthEmulator(firebaseClientAuth, "http://127.0.0.1:9099");
-// }
+const getFirebaseEmulatorURL = () => {
+    if(process.env.NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST) return process.env.NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST;
+    throw new Error("provide FIREBASE_AUTH_EMULATOR_HOST variable in the development env")
+}
+
+if (process.env.NODE_ENV === 'development') {
+    // Use emulator if developing
+    connectAuthEmulator(firebaseClientAuth, getFirebaseEmulatorURL());
+}
 
 // UI instance for login / signup
 export const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebaseClientAuth);
