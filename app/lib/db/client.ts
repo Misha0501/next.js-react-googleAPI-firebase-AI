0d79fc3f5378b944a99e9d140b7aa1e8c374c565
mykhailogalenda <misha.galenda@gmail.com>
2023-08-29 22:37:25 +0200

    Added soft delete for listings

--- app/lib/db/client.ts
+++ app/lib/db/client.ts
@@ -10,7 +10,7 @@ const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
         {model: "Listing"},
     ],
     storage: {type: "redis", options: {client: redis, invalidation: {referencesTTL: 300}, log: console}},
-    cacheTime: 300,
+    cacheTime: 20,
     onHit: (key) => {
         console.log("hit", key);
     },
@@ -21,6 +21,31 @@ const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
         console.log("error", key);
     },
 });
+
+
+// Prisma use soft delete for listing table
+prismaClient.$use(async (params, next) => {
+    // Check incoming query type
+    if (params.model == 'Listing') {
+        if (params.action == 'delete') {
+            // Delete queries
+            // Change action to an update
+            params.action = 'update'
+            params.args['data'] = { deleted: new Date() }
+        }
+        if (params.action == 'deleteMany') {
+            // Delete many queries
+            params.action = 'updateMany'
+            if (params.args.data != undefined) {
+                params.args.data['deleted'] = new Date()
+            } else {
+                params.args['data'] = { deleted: new Date() }
+            }
+        }
+    }
+    return next(params)
+})
+
 
 prismaClient.$use(cacheMiddleware);
 
