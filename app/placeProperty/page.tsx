b2eb664f034f785b8012ac2254468ee21c4d360e
misha0501 <misha.galenda@gmail.com>
2023-09-06 21:04:32 +0200

    refactored places property code

--- app/placeProperty/page.tsx
+++ app/placeProperty/page.tsx
@@ -1,19 +1,17 @@
-"use client"
+"use client";
 
-
-import {PlacePropertyForm} from "@/app/components/PlacePropertyForm";
+import { PlacePropertyForm } from "@/app/components/PlacePropertyForm";
 
 function PlacePropertyPage() {
-    return (
-        <>
-            <section className="py-32">
-                <div className="container">
-                    <PlacePropertyForm></PlacePropertyForm>
-                </div>
-
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="py-32">
+        <div className="container">
+          <PlacePropertyForm></PlacePropertyForm>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default PlacePropertyPage;
