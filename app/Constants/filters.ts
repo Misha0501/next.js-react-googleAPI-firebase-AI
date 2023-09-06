13155b308f0df58008ec18bdedfa66162a54e7cb
misha0501 <misha.galenda@gmail.com>
2023-09-06 21:23:58 +0200

    refactored validators

---
+++ app/Constants/filters.ts
@@ -0,0 +1,104 @@
+export const propertyTypesInitialState = [
+  { value: "1", label: "Residential building", checked: false },
+  { value: "2", label: "Apartment", checked: false },
+];
+export const NO_MAX = "No max";
+
+export const areaLivingMinOptions = [
+  "0",
+  "50",
+  "75",
+  "100",
+  "125",
+  "150",
+  "175",
+  "200",
+  "250",
+];
+export const areaLivingMaxOptions = [
+  "50",
+  "75",
+  "100",
+  "125",
+  "150",
+  "175",
+  "200",
+  "250",
+];
+
+export const areaLandMinOptions = [
+  "0",
+  "100",
+  "150",
+  "200",
+  "250",
+  "300",
+  "350",
+  "400",
+  "450",
+  "500",
+];
+export const areaLandMaxOptions = [
+  "100",
+  "150",
+  "200",
+  "250",
+  "300",
+  "350",
+  "400",
+  "450",
+  NO_MAX,
+];
+
+const propertyMinPriceBuy = [
+  "0",
+  "50000",
+  "75000",
+  "100000",
+  "125000",
+  "150000",
+];
+const propertyMaxPriceBuy = [
+  "50000",
+  "75000",
+  "100000",
+  "125000",
+  "150000",
+  NO_MAX,
+];
+export const minRoomsOptions = ["0", "1", "2", "3", "4", "5"];
+export const maxRoomsOptions = ["1", "2", "3", "4", "5", NO_MAX];
+export const minBedroomsOptions = ["0", "1", "2", "3", "4", "5"];
+export const maxBedroomsOptions = ["1", "2", "3", "4", "5", NO_MAX];
+export const listedSinceOptions = [
+  {
+    label: "No preference",
+    checked: true,
+    value: "noPref",
+  },
+  {
+    label: "Last 24 hours",
+    checked: false,
+    value: "1",
+  },
+  {
+    label: "3 days",
+    checked: false,
+    value: "3",
+  },
+  {
+    label: "7 days",
+    checked: false,
+    value: "7",
+  },
+  {
+    label: "10 days",
+    checked: false,
+    value: "10",
+  },
+  {
+    label: "30 days",
+    checked: false,
+    value: "30",
+  },
+];
--- app/Constants/index.ts
+++ app/Constants/index.ts
@@ -6,8 +6,8 @@ import {
   UpkeepType,
 } from "@/types";
 
-export const HEATING_TYPE: HeatingType[] = ["CENTRAL", "BOILER"];
-export const INTERIOR_TYPE: InteriorType[] = ["FURNISHED", "UNFURNISHED"];
-export const UPKEEP_TYPE: UpkeepType[] = ["EXCELLENT", "GOOD", "FAIR", "POOR"];
-export const LISTING_TYPE: ListingType[] = ["RENT", "SELL"];
-export const CURRENCY: CurrencyType[] = ["EUR", "USD", "BGN"];
+export const HEATING_TYPES: HeatingType[] = ["CENTRAL", "BOILER"];
+export const INTERIOR_TYPES: InteriorType[] = ["FURNISHED", "UNFURNISHED"];
+export const UPKEEP_TYPES: UpkeepType[] = ["EXCELLENT", "GOOD", "FAIR", "POOR"];
+export const LISTING_TYPES: ListingType[] = ["RENT", "SELL"];
+export const CURRENCIES: CurrencyType[] = ["EUR", "USD", "BGN"];
