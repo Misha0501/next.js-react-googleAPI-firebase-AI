199865313da783a5accf80083d6267ace35f66f3
misha0501 <misha.galenda@gmail.com>
2023-09-09 23:49:15 +0200

    updated sign in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,49 +1,11 @@
-"use client";
+import SignInPageContent from "@/app/components/SignInPageContent";
 import "firebaseui/dist/firebaseui.css";
-import { useEffect, useRef } from "react";
-import firebase from "firebase/compat/app";
-import * as firebaseui from "firebaseui";
-import { firebaseClientAuth } from "@/app/lib/firebase/configClient";
 
-export default function FirebaseUi() {
-
-  // UI instance for login / signup
-  const loading = useRef(null);
-
-  useEffect(() => {
-    let ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebaseClientAuth);
-
-    ui.start("#firebaseui", {
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-      ],
-      signInSuccessUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/profile/myProperties`,
-      // Other config options...
-      callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-          // User successfully signed in.
-          // Return type determines whether we continue the redirect automatically
-          // or whether we leave that to developer to handle.
-          return true;
-        },
-        uiShown: function() {
-          // The widget is rendered.
-          // Hide the loader.
-          if (!loading || !loading.current) return;
-
-          loading.current.style.display = "none";
-
-        }
-      }
-    });
-  }, []);
+export default function SignInPage() {
 
   return (
     <>
-      <h1 className="text-5xl flex justify-center mb-5">Login</h1>
-      <div id="firebaseui" className="firebaseui"></div>
-      <div ref={loading}>Loading...</div>
+      <SignInPageContent />
     </>
   );
 }
