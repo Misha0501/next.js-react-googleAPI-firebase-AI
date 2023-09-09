aaf98bf8a4742c93c762296eeab5a228790b00a7
misha0501 <misha.galenda@gmail.com>
2023-09-09 22:16:19 +0200

    added key to the listing item

--- app/components/ProfilePageOwnListings.tsx
+++ app/components/ProfilePageOwnListings.tsx
@@ -10,8 +10,8 @@ export const ProfilePageOwnListings = ({listings}: Props) => {
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-3 gap-4">
-        {listings && listings.map((listing) => (
-          <ListingItem listingItem={listing} ></ListingItem>
+        {listings && listings.map((listing, index) => (
+          <ListingItem key={index} listingItem={listing} ></ListingItem>
         ))}
       </div>
     </div>
