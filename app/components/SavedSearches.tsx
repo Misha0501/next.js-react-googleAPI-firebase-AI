fd0c5ba708b184762b77adfd61875a842edf9e7c
misha0501 <misha.galenda@gmail.com>
2023-09-15 21:10:14 +0200

    added data to saved searches

--- app/components/SavedSearches.tsx
+++ app/components/SavedSearches.tsx
@@ -5,6 +5,7 @@ import { getFetchUrl } from "@/app/lib/getFetchUrl";
 import { useAuthContext } from "@/app/context/AuthContext";
 import { SavedSearch } from "@/types";
 import { XMarkIcon } from "@heroicons/react/24/solid";
+
 export const SavedSearches = () => {
   const { authToken } = useAuthContext();
 
@@ -57,36 +58,54 @@ export const SavedSearches = () => {
 
         <div className={"flex flex-col gap-12"}>
           {savedSearches && savedSearches.map((item, index) => (
-            <div className="p-8 flex flex-col bg-[#F7F9FC] hover:bg-[#ebeef2] hover:cursor-pointer rounded-xl" key={index}>
+            <div className="p-8 flex flex-col bg-[#F7F9FC] hover:bg-[#ebeef2] hover:cursor-pointer rounded-xl"
+                 key={index}>
               <div className="flex justify-between py-3 font-bold text-xl">
                 {item.locality && <span className={"w-1/2"}>{item.locality}</span>}
                 {!item.locality && <span className={"w-1/2"}>Bulgaria</span>}
-                <XMarkIcon className={"h-6 w-6"}/>
+                <XMarkIcon className={"h-6 w-6"} />
               </div>
-              <div className="border-b flex justify-between py-3">
+              {(item.priceMin || item.priceMax) &&
+                <div className="border-b flex justify-between py-3">
                 <span className={"w-1/2 text-gray-500"}>Price</span>
-                <span className={"w-1/2"}>x</span>
-              </div>
-              <div className="border-b flex justify-between py-3">
-                <span className={"w-1/2 text-gray-500"}>Listed Since</span>
-                <span className={"w-1/2"}>x</span>
-              </div>
+                <span className={"w-1/2"}>From {item.priceMin || "-"} to {item.priceMax || "-"}</span>
+              </div>}
+              {item.listingType &&
               <div className="border-b flex justify-between py-3">
                 <span className={"w-1/2 text-gray-500"}>Listing type</span>
-                <span className={"w-1/2"}>For sale</span>
+                <span className={"w-1/2"}>{item.listingType === "SELL" ? "Buy" : "Rent"}</span>
               </div>
+              }
+              {(item.propertyType && item.propertyType.length > 0) &&
+                <div className="border-b flex justify-between py-3">
+                  <span className={"w-1/2 text-gray-500"}>Property types</span>
+                  <span className={"w-1/2"}>{item.propertyType.map(value => value + "  ")}</span>
+                </div>
+              }
+              {(item.areaTotalMin || item.areaTotalMax) &&
               <div className="border-b flex justify-between py-3">
-                <span className={"w-1/2 text-gray-500"}>Square meters living area</span>
-                <span className={"w-1/2"}>from 50</span>
+                <span className={"w-1/2 text-gray-500"}>Area total</span>
+                <span className={"w-1/2"}>From {item.areaTotalMin || "-"} to {item.areaTotalMax || "-"}</span>
               </div>
+              }
+              {(item.areaLivingMin || item.areaLivingMax) &&
+                <div className="border-b flex justify-between py-3">
+                  <span className={"w-1/2 text-gray-500"}>Area living</span>
+                  <span className={"w-1/2"}>From {item.areaLivingMin || "-"} to {item.areaLivingMax || "-"}</span>
+                </div>
+              }
+              {(item.roomsMin || item.roomsMax) &&
               <div className="border-b flex justify-between py-3">
                 <span className={"w-1/2 text-gray-500"}>Rooms</span>
-                <span className={"w-1/2"}>from 50</span>
+                <span className={"w-1/2"}>From {item.roomsMin || "-"} to {item.roomsMax || "-"}</span>
               </div>
-              <div className="border-b flex justify-between py-3">
+              }
+              {(item.bedroomsMin || item.bedroomsMax) &&
+                <div className="border-b flex justify-between py-3">
                 <span className={"w-1/2 text-gray-500"}>Bedrooms</span>
-                <span className={"w-1/2"}>from 50</span>
+                  <span className={"w-1/2"}>From {item.bedroomsMin || "-"} to {item.bedroomsMax || "-"}</span>
               </div>
+              }
               <div className="border-b flex justify-between py-3">
                 <span className={"w-1/2 text-gray-500"}>Search created</span>
                 <span className={"w-1/2"}>{item.createdAt}</span>
