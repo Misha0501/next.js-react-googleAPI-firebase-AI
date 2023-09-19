d7782261f1eb771c3da3829a51a7093bb507e659
misha0501 <misha.galenda@gmail.com>
2023-09-19 22:45:28 +0200

    adjusted api url

--- app/lib/getFetchUrl.ts
+++ app/lib/getFetchUrl.ts
@@ -10,7 +10,7 @@ export const getFetchUrl = (route: string) => {
 
     return `${
       process.env.NODE_ENV === "production"
-        ? `https://${process.env.API_URL || process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL }` || "http://localhost:3000" 
+        ? `https://${process.env.NEXT_PUBLIC_API_URL || process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'}` 
         : "http://localhost:3000"
     }${route}`;
 };
