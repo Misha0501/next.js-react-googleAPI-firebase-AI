93e993d95f77456e7713ae419ee2b992128b003e
misha0501 <misha.galenda@gmail.com>
2023-09-19 21:21:43 +0200

    updated get fetch url

--- app/lib/getFetchUrl.ts
+++ app/lib/getFetchUrl.ts
@@ -1,10 +1,10 @@
 export const getFetchUrl = (route: string) => {
     // Temporary fix for Vercel deployment issue TODO: remove this when fixed
-    return "http://localhost:3000/" + route;
+    // return "http://localhost:3000/" + route;
 
     return `${
       process.env.NODE_ENV === "production"
-        ? process.env.VERCEL_URL!
+        ? process.env.VERCEL_URL || "http://localhost:3000" 
         : "http://localhost:3000"
     }/${route}`;
 };
