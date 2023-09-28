cbde56738cd72107783f0e8ba0d1da4f18ff2df6
misha0501 <misha.galenda@gmail.com>
2023-09-28 12:42:47 +0200

    test

--- __checks__/real-estate-website.spec.ts
+++ __checks__/real-estate-website.spec.ts
@@ -4,14 +4,16 @@ import { test, expect } from '@playwright/test'
 // test.setTimeout(60_000);
 
 test('Custom Browser Check', async ({ page }) => {
-  const response = await page.goto('https://real-estate-website-two-eta.vercel.app/')
+  const targetUrl = process.env.ENVIRONMENT_URL || 'https://real-estate-website-two-eta.vercel.app/'
+  const response = await page.goto(targetUrl)
   expect(response?.status()).toBeLessThan(400)
   await page.screenshot({ path: 'screenshot.jpg' })
 })
 
 
 test('Custom Browser Check Two', async ({ page }) => {
-  const response = await page.goto('https://real-estate-website-two-eta.vercel.appsdf/')
+  const targetUrl = process.env.ENVIRONMENT_URL
+  const response = await page.goto(targetUrl)
   expect(response?.status()).toBeLessThan(400)
   await page.screenshot({ path: 'screenshot.jpg' })
 })
