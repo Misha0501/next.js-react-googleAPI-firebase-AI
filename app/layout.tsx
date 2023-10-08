459f047c37eff66c61b0537d3606675ec971a953
misha0501 <misha.galenda@gmail.com>
2023-10-08 15:13:42 +0200

    temporally deleted footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
             <ReactQueryProvider>{children}</ReactQueryProvider>
           </AuthContextProvider>
         </main>
-        <Footer />
       </body>
     </html>
   );
