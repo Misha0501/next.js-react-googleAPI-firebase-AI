1dac54b599dd4cc5ee212d2d9fc8a70b6158ae3a
misha0501 <misha.galenda@gmail.com>
2023-10-08 14:11:53 +0200

    deployed tests

--- __checks__/real-estate-website.check.ts
+++ __checks__/real-estate-website.check.ts
@@ -4,25 +4,38 @@
 * - https://www.checklyhq.com/docs/cli/constructs-reference/#browsercheck
 */
 
-import { BrowserCheck, Frequency, RetryStrategyBuilder } from 'checkly/constructs'
+import { BrowserCheck, CheckGroup, Frequency, RetryStrategyBuilder } from "checkly/constructs";
 
-new BrowserCheck('real-estate-website', {
-  name: 'real-estate-website',
+const group = new CheckGroup('check-group-11', {
+  name: 'Group main functionality',
   activated: true,
-  muted: false,
-  shouldFail: false,
-  locations: ['eu-central-1', 'eu-west-2'],
-  tags: [],
-  sslCheckDomain: '',
-  frequency: Frequency.EVERY_24H,
-  environmentVariables: [],
-  code: {
-    entrypoint: './real-estate-website.spec.ts',
-  },
-  retryStrategy: RetryStrategyBuilder.fixedStrategy({
-    baseBackoffSeconds: 0,
-    maxRetries: 1,
-    maxDurationSeconds: 600,
-    sameRegion: false,
-  }),
+  frequency: Frequency.EVERY_15M,
+  locations: ['us-east-1', 'eu-west-1'],
+  tags: ['api-group'],
+  concurrency: 10,
+  browserChecks: {
+    frequency: Frequency.EVERY_30M,
+    testMatch: '*.spec.js'
+  }
 })
+
+// new BrowserCheck('real-estate-website', {
+//   name: 'real-estate-website',
+//   activated: true,
+//   muted: false,
+//   shouldFail: false,
+//   locations: ['eu-central-1', 'eu-west-2'],
+//   tags: [],
+//   sslCheckDomain: '',
+//   frequency: Frequency.EVERY_24H,
+//   environmentVariables: [],
+//   code: {
+//     entrypoint: './real-estate-website.spec.ts',
+//   },
+//   retryStrategy: RetryStrategyBuilder.fixedStrategy({
+//     baseBackoffSeconds: 0,
+//     maxRetries: 1,
+//     maxDurationSeconds: 600,
+//     sameRegion: false,
+//   }),
+// })
