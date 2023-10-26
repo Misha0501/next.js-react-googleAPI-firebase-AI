e797f563bdeca23cef635b141343e97fa9644457
misha0501 <misha.galenda@gmail.com>
2023-10-26 20:49:17 +0200

    updated fetch url to have a distinction between client and server side

--- app/lib/getFetchUrl.ts
+++ app/lib/getFetchUrl.ts
@@ -3,9 +3,15 @@
  * @param route Route to get the fetch url for
  */
 export const getFetchUrl = (route: string) => {
-    return `${
-      process.env.NODE_ENV === "production"
-        ? `https://${process.env.NEXT_PUBLIC_API_URL || process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'}` 
-        : "http://localhost:3000"
-    }${route}`;
+    if (typeof window === 'undefined') {
+        // server-side, fallback to the environment variables or localhost
+        return `${
+          process.env.NODE_ENV === "production"
+            ? `https://${process.env.NEXT_PUBLIC_API_URL || process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'}`
+            : "http://localhost:3000"
+        }${route}`;
+    }
+
+    // client-side, use current website's base URL
+    return `${window.location.origin}${route}`;
 };
