9dea099e99bd6bd45c0f6066395b30e986d007ae
misha0501 <misha.galenda@gmail.com>
2023-10-27 16:58:55 +0200

    refactored contact form API route

---
+++ app/api/contactForm/_utils.ts
@@ -0,0 +1,44 @@
+/**
+ * Gets the default email address if none is provided.
+ * @param {string | undefined} emailTo - The provided email address.
+ * @returns {string} - Returns the email address or a default one if none is provided.
+ */
+export const getDefaultEmailTo = (emailTo?: string): string => {
+  return emailTo ? emailTo : "m.galenda5@gmail.com";
+};
+
+/**
+ * Gets the default subject if none is provided.
+ * @param {string | undefined} subject - The provided subject.
+ * @returns {string} - Returns the subject or a default one if none is provided.
+ */
+export const getDefaultSubject = (subject?: string): string => {
+  return subject ? subject : "Contact Form";
+};
+
+/**
+ * Creates the HTML content for the email.
+ * @param {string} name - The name of the sender.
+ * @param {string} phoneNumber - The phone number of the sender.
+ * @param {string} email - The email of the sender.
+ * @param {string} subject - The subject of the email.
+ * @param {string} message - The message content.
+ * @returns {string} - Returns the HTML content for the email.
+ */
+export const createEmailHTMLContent = (
+  name: string,
+  phoneNumber: string | undefined,
+  email: string,
+  subject: string,
+  message: string
+): string => {
+  return `
+        <h1>Hi there!</h1>
+        <p>You have a new message from ${name}.</p>
+        <p>Phone number: ${phoneNumber ?? '-'}</p>
+        <p>Email: ${email}</p>
+        <p>Subject: ${subject}</p>
+        <p>Message:</p>
+        <p>${message}</p>
+    `;
+};
--- app/api/contactForm/route.ts
+++ app/api/contactForm/route.ts
@@ -1,51 +1,35 @@
-import { ResponseError } from "@/app/lib/classes/ResponseError";
 import { contactFormSchema } from "@/app/lib/validations/contactForm";
 import { NextResponse } from "next/server";
-import { z } from "zod";
 import { sendEmail } from "@/app/lib/email";
+import { handleAPIError } from "@/app/lib/api/handleError";
+import {
+  createEmailHTMLContent,
+  getDefaultEmailTo,
+  getDefaultSubject,
+} from "@/app/api/contactForm/_utils";
 
 export const dynamic = "force-dynamic"; // Force dynamic (server) route instead of static page
 
 export async function POST(req: Request) {
   try {
     const parsedValues = contactFormSchema.parse(await req.json());
-    let { name, phoneNumber, email, message, emailTo, subject  } =
+    let { name, phoneNumber, email, message, emailTo, subject } =
       parsedValues;
 
-    // Assing emailTo to email if emailTo is not set
-    emailTo = emailTo ? emailTo : "m.galenda5@gmail.com";
-
-    // Assign subject to "Contact Form" if subject is not set
-    subject = subject ? subject : "Contact Form";
+    emailTo = getDefaultEmailTo(emailTo);
+    subject = getDefaultSubject(subject);
+    
+    const emailContent = createEmailHTMLContent(name, phoneNumber, email, subject, message);
 
     // Send email
     await sendEmail({
       to: emailTo,
       subject,
-      html: `
-            <h1>Hi there!</h1>
-            <p>You have a new message from ${name}.</p>
-            <p>Phone number: ${phoneNumber}</p>
-            <p>Email: ${email}</p>
-            <p>Subject: ${subject}</p>
-            <p>Message:</p>
-            <p>${message}</p>
-          `
+      html: emailContent
     });
 
     return NextResponse.json(parsedValues);
   } catch (error) {
-    console.error(error);
-    if (error instanceof z.ZodError) {
-      return new Response(error.message, { status: 422 });
-    }
-
-    if (error instanceof ResponseError) {
-      return new Response(error.message, { status: error.status });
-    }
-
-    return new Response("Something went wrong please try again later", {
-      status: 500,
-    });
+    return handleAPIError(error);
   }
 }
