2f2fc208d3ea580909efe5516265a7ed9b650ed9
misha0501 <misha.galenda@gmail.com>
2023-10-27 18:36:44 +0200

    added JSDoc

--- app/api/recentlyViewedListings/_utils.ts
+++ app/api/recentlyViewedListings/_utils.ts
@@ -1,5 +1,11 @@
 import { prisma } from "@/app/lib/db/client";
 
+/**
+ * Fetches the recently viewed listings for a specific user.
+ *
+ * @param {number} userId - The ID of the user.
+ * @returns {Promise<{total: number, results: any[]}>} - An object containing the total number of items and the actual listing items (up to 10).
+ */
 export const getRecentlyViewedListings = async (
   userId: number,
 ): Promise<any> => {
@@ -31,6 +37,15 @@ export const getRecentlyViewedListings = async (
   return { total: itemsCount, results: items };
 };
 
+/**
+ * Handles the creation or retrieval of a recently viewed listing record for a user.
+ * If the listing was already recently viewed by the user, the existing record is returned.
+ * Otherwise, a new record is created.
+ *
+ * @param {number} userId - The ID of the user.
+ * @param {number} listingId - The ID of the listing.
+ * @returns {Promise<any>} - The recently viewed listing record.
+ */
 export const handleRecentlyViewedCreation = async (
   userId: number,
   listingId: number,
--- app/api/savedListings/_utils.ts
+++ app/api/savedListings/_utils.ts
@@ -1,6 +1,12 @@
 import { prisma } from "@/app/lib/db/client";
 import { ResponseError } from "@/app/lib/classes/ResponseError";
 
+/**
+ * Retrieves the saved listings for a specific user.
+ *
+ * @param {number} userId - The ID of the user.
+ * @returns {Promise<{total: number, results: any[]}>} - An object containing the total number of saved listings and the saved listings themselves.
+ */
 export const getSavedListings = async (userId: number): Promise<any> => {
   const savedListingsCount = await prisma.savedListing.count({
     where: {
@@ -26,6 +32,15 @@ export const getSavedListings = async (userId: number): Promise<any> => {
   return { total: savedListingsCount, results: savedListings };
 };
 
+/**
+ * Handles the creation or retrieval of a saved listing record for a user.
+ * If the listing was already saved by the user, the existing record is returned.
+ * Otherwise, a new saved listing record is created.
+ *
+ * @param {number} userId - The ID of the user.
+ * @param {number} listingId - The ID of the listing.
+ * @returns {Promise<any>} - The saved listing record.
+ */
 export const handleSavedListingCreation = async (
   userId: number,
   listingId: number,
@@ -46,12 +61,26 @@ export const handleSavedListingCreation = async (
   return existingSavedListing[0];
 };
 
+/**
+ * Fetches a specific saved listing by its ID.
+ *
+ * @param {number} id - The ID of the saved listing.
+ * @returns {Promise<any>} - The saved listing record.
+ */
 export const fetchSavedListing = async (id: number): Promise<any> => {
   return await prisma.savedListing.findUnique({
     where: { id },
   });
 };
 
+/**
+ * Validates if a user is authorized to perform operations on a specific saved listing.
+ * Throws an error if the user is not authorized.
+ *
+ * @param {number} applicationUserId - The ID of the application user.
+ * @param {any} savedListing - The saved listing object.
+ * @throws {ResponseError} - Throws error if user is not authorized.
+ */
 export const validateUserAuthorization = (
   applicationUserId: number,
   savedListing: any,
@@ -61,6 +90,12 @@ export const validateUserAuthorization = (
   }
 };
 
+/**
+ * Deletes a saved listing based on its ID.
+ *
+ * @param {number} id - The ID of the saved listing to be deleted.
+ * @returns {Promise<void>}
+ */
 export const deleteSavedListing = async (id: number): Promise<void> => {
   await prisma.savedListing.delete({
     where: { id },
