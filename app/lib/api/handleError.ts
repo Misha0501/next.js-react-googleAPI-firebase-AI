62076e8c13968ff85e7670aa947141685d4fc657
misha0501 <misha.galenda@gmail.com>
2023-10-27 18:42:20 +0200

    added JSDoc

--- app/lib/api/handleError.ts
+++ app/lib/api/handleError.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 import { ResponseError } from "@/app/lib/classes/ResponseError";
 
+/**
+ * Handles errors from API operations, returning a standardized response.
+ * It supports Zod validation errors, custom response errors, and authentication errors.
+ * If the error type is not recognized, it defaults to a generic server error.
+ *
+ * @param {any} error - The error object thrown during API operations.
+ * @returns {Response} - The constructed response object based on the error.
+ */
 export const handleAPIError = (error: any): Response => {
   console.error(error);
   if (error instanceof z.ZodError) {
@@ -10,9 +18,12 @@ export const handleAPIError = (error: any): Response => {
     return new Response(error.message, { status: error.status });
   }
   if (error.errorInfo && error.errorInfo.code) {
-    return new Response('Your auth token is invalid or it has expired. Get a new auth token and try again.', { status: 400 });
+    return new Response(
+      "Your auth token is invalid or it has expired. Get a new auth token and try again.",
+      { status: 400 },
+    );
   }
-  return new Response('Something went wrong please try again later', {
+  return new Response("Something went wrong please try again later", {
     status: 500,
   });
 };
--- app/lib/api/validateParamId.ts
+++ app/lib/api/validateParamId.ts
@@ -1,7 +1,15 @@
 import { ResponseError } from "@/app/lib/classes/ResponseError";
 
+/**
+ * Validates a given slug to ensure it can be converted to a valid ID number.
+ * If the slug cannot be converted to a number or is NaN, it throws a custom ResponseError.
+ *
+ * @param {number} slug - The slug (usually from a URL) to be validated and converted.
+ * @returns {number} - The validated ID as a number.
+ * @throws {ResponseError} - If the slug is not a valid number.
+ */
 export const validateParamId = (slug: number): number => {
   const id = Number(slug);
   if (isNaN(id)) throw new ResponseError("ID must be a valid number", 422);
   return id;
-}
+};
