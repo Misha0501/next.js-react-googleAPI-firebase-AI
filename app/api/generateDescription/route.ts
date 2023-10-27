70b009838e034e91a857a75d9a42cb1a92dd4647
misha0501 <misha.galenda@gmail.com>
2023-10-27 17:39:07 +0200

    refactored generate description API route error handling

--- app/api/generateDescription/route.ts
+++ app/api/generateDescription/route.ts
@@ -1,14 +1,12 @@
 import { NextResponse } from "next/server";
-import openAI from "@/app/lib/openAI";
 import OpenAI from "openai";
-import { ResponseError } from "@/app/lib/classes/ResponseError";
-import * as z from "zod";
-import {openAISchema} from "@/app/lib/validations/openAI";
+import { openAISchema } from "@/app/lib/validations/openAI";
 import {
   generateUserMessageContent,
   getOpenAICompletion,
-  validateOpenAIResponse
+  validateOpenAIResponse,
 } from "@/app/api/generateDescription/_utils";
+import { handleAPIError } from "@/app/lib/api/handleError";
 
 export async function POST(request: Request) {
   try {
@@ -24,28 +22,11 @@ export async function POST(request: Request) {
 
     return NextResponse.json(content);
   } catch (error) {
-    console.error(error);
-    if (error instanceof z.ZodError) {
-      return new Response(error.message, { status: 422 });
-    }
-
-    if (error instanceof ResponseError) {
-      return new Response(error.message, { status: error.status });
-    }
-
-    if (error.errorInfo && error.errorInfo.code) {
-      return new Response(
-        "Your auth token is invalid or it has expired. Get a new auth token and try again.",
-        { status: 400 },
-      );
-    }
-
     if (error instanceof OpenAI.APIError) {
+      console.error(error);
       return new Response(error.name, { status: error.status });
     }
 
-    return new Response("Something went wrong, please try again later.", {
-      status: 500,
-    });
+    return handleAPIError(error);
   }
 }
