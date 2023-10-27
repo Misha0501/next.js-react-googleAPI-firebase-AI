96e156d9f4fc386f70753b2e9524236f2c3b1f61
misha0501 <misha.galenda@gmail.com>
2023-10-27 18:32:46 +0200

    added JSDoc

--- app/api/images/[slug]/_utils.ts
+++ app/api/images/[slug]/_utils.ts
@@ -4,7 +4,16 @@ import { ApplicationUser } from "@/types";
 import { getApplicationUserCompanyId } from "@/app/lib/listing/getApplicationUserCompanyId";
 import { userAllowedManipulateListing } from "@/app/lib/listing/userAllowedManipulateListing";
 
-export const fetchImageById = async (id: number) => {
+/**
+ * Fetches an image by its ID and also retrieves the associated listing ID.
+ *
+ * @param {number} id - The ID of the image to fetch.
+ * @returns {Promise<{image: any, listingId: number | undefined}>} - An object containing the image and its associated listing ID.
+ * @throws {ResponseError} - Throws an error if the associated listing ID is not found.
+ */
+export const fetchImageById = async (
+  id: number,
+): Promise<{ image: any; listingId: number | undefined }> => {
   const image = await prisma.listingImage.findUnique({
     where: {
       id,
@@ -19,10 +28,18 @@ export const fetchImageById = async (id: number) => {
   return { image, listingId };
 };
 
+/**
+ * Ensures the user has permissions for a specific listing.
+ *
+ * @param {ApplicationUser} applicationUser - The application user object.
+ * @param {number} listingId - The ID of the listing to check permissions for.
+ * @returns {Promise<void>} - Resolves if the user has permissions, otherwise throws an error.
+ * @throws {ResponseError} - Throws an error if the listing is not found or the user does not have the necessary permissions.
+ */
 export const ensureListingAndUserPermissions = async (
   applicationUser: ApplicationUser,
   listingId: number,
-) => {
+): Promise<void> => {
   const listing = await prisma.listing.findUnique({
     where: {
       id: listingId,
@@ -46,7 +63,13 @@ export const ensureListingAndUserPermissions = async (
   }
 };
 
-export const deleteImageFromDB = async (id: number) => {
+/**
+ * Deletes an image from the database by its ID.
+ *
+ * @param {number} id - The ID of the image to delete.
+ * @returns {Promise<void>} - Resolves once the image is deleted.
+ */
+export const deleteImageFromDB = async (id: number): Promise<void> => {
   await prisma.listingImage.delete({
     where: { id },
   });
