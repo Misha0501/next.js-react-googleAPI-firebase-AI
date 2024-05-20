e9f816e809580d6fc37153c0ec6d97b1b962d3f3
misha0501 <misha.galenda@gmail.com>
2024-05-20 15:13:14 +0400

    changed the logo

--- app/components/layout/Navigation.tsx
+++ app/components/layout/Navigation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import logo from "@/public/BoraLogo.png";
+import logo from "@/public/logo.png";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@tremor/react";
@@ -47,8 +47,8 @@ export const Navigation = () => {
                     <Link href="/">
                       <Image
                         className={"w-24"}
-                        width={60}
-                        height={35}
+                        width={100}
+                        height={65}
                         src={logo}
                         alt="Company logo"
                         placeholder="blur" // Optional blur-up while loading
