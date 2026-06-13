d19fc703cd4c318f7e09522a712f7cc2cdcc6a7a
Mykhailo Galenda <mykhailo.galenda@poolder.com>
2026-06-13 19:12:08 +0200

    removed "Contact" button from navigation

--- app/components/layout/Navigation.tsx
+++ app/components/layout/Navigation.tsx
@@ -56,9 +56,6 @@ export const Navigation = () => {
                   </div>
                   <div className="hidden sm:ml-6 sm:block">
                     <div className="flex space-x-4 items-center">
-                      <Link href="/contact">
-                        <Button variant="light">Contact</Button>
-                      </Link>
                       <Link href="/profile/saved">
                         <Button variant="secondary">Saved properties</Button>
                       </Link>
@@ -83,16 +80,6 @@ export const Navigation = () => {
             <div className="relative duration-700">
               <Disclosure.Panel className="absolute w-full p-2 top-0 left-0 z-40 bg-white duration-700 border-t-2 sm:hidden shadow-[0_35px_60px_-10px_rgba(167,165,165,0.3)] ">
                 <div className="space-y-1 px-2 pb-3 pt-2 flex flex-col items-end gap-3">
-                  <Link href="/contact">
-                    <Button
-                      variant="light"
-                      onClick={() => {
-                        close();
-                      }}
-                    >
-                      Contact
-                    </Button>
-                  </Link>
                   <Link href="/profile/saved">
                     <Button
                       variant="secondary"
