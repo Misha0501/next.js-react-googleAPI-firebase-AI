b77e3d325a7b4beab4896c7f5b13d8c3dfb431bc
Mykhailo Galenda <mykhailo.galenda@poolder.com>
2026-06-14 13:52:37 +0200

    wrapped `ListingsPageContent` in `Suspense` to improve loading state handling

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import { ListingsPageContent } from "@/app/components/listingsPage/ListingsPageContent";
 
 export default function Listings() {
   return (
-    <>
-      <ListingsPageContent/>
-    </>
+    <Suspense>
+      <ListingsPageContent />
+    </Suspense>
   );
 }
